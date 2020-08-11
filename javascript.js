953abab51ac5b3f829eb933fd0f5cc8c818408db
unknown <smraselcoxr@gmail.com>
2020-08-11 18:27:46 +0600

    Nice example of setTimeout and math game project

--- javascript.js
+++ javascript.js
@@ -80,18 +80,8 @@ for(i=1; i<5; i++){
             }, 1000);
         }
     }
-}   
+  }   
 }
-//if we click on answer box
-    //if we are playing
-        //correct?
-            //yes
-                //increase score
-                //show correct box for 1sec
-                //generate new Q&A
-            //no
-                //show try again box for 1sec
-
 
 //functions
 
